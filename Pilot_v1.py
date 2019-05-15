6e39b7065704ca58eeb7d3ccf4f6c8d8d2350042
Georgeye458 <32994701+Georgeye458@users.noreply.github.com>
2019-05-15 19:27:14 +1000

    Test files and test recognition

--- Pilot_v1.py
+++ Pilot_v1.py
@@ -10,12 +10,21 @@ pd.set_option('display.max_columns', 300)
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Talk into Microphone')
-    audio = r.listen(source)
+# This is an implementation with the mircrophone. Let's try one without.
+# with sr.Microphone() as source:
+#     print('Talk into Microphone')
+#     audio = r.listen(source)
 
-    try:
-        text = recognize_google(audio)
-        print('You said : {}'.format(text))
-    except:
-        print('Cannot recognise your voice')
+#     try:
+#         text = recognize_google(audio) #Can use recognize_sphinx() if you want offline.
+#         print('You said : {}'.format(text))
+#     except:
+#         print('Cannot recognise your voice')
+
+stocks_audio = sr.AudioFile('stocks.wav')
+
+with stocks_audio as source:
+    audio = r.record(source, duration=10)
+
+text = r.recognize_google(audio)
+print(text)
