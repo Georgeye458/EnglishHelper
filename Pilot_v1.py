b6d17a7040e6850d28534bb4e1b925ccc151dbfe
Georgeye458 <32994701+Georgeye458@users.noreply.github.com>
2019-05-15 19:04:25 +1000

    Speech Recognition attempt

--- Pilot_v1.py
+++ Pilot_v1.py
@@ -1,7 +1,21 @@
 #import modules
 import pandas as pd
 import numpy as np
+import speech_recognition as sr
+
 
 pd.set_option('display.max_rows', 15)
 pd.set_option('display.max_columns', 300)
 
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Talk into Microphone')
+    audio = r.listen(source)
+
+    try:
+        text = recognize_google(audio)
+        print('You said : {}'.format(text))
+    except:
+        print('Cannot recognise your voice')
